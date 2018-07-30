664d2862796af91e5fd958cce651da20468f8bf2
Tom Elliott <tom.elliott@nyu.edu>
2018-07-30 15:00:49 -0500

    stub out images module and its test module, including shell for MoondogImage class

---
+++ moondog/images.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Manage image collection
+"""
+
+import better_exceptions
+import logging
+
+logger = logging.getLogger(__name__)
+
+class MoondogImage:
+
+    def init(self):
+        pass
+
