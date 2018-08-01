b8185d0b91c8f6bb831101cba2eb50526aff0f17
Tom Elliott <tom.elliott@nyu.edu>
2018-08-01 15:12:00 -0500

    basic image package construction using bagit

--- moondog/images.py
+++ moondog/images.py
@@ -4,8 +4,11 @@
 Manage image collection
 """
 
+from bagit import Bag, BagError, make_bag
 import better_exceptions
 import logging
+from os import makedirs
+from os.path import expanduser, expandvars, normpath, realpath
 
 logger = logging.getLogger(__name__)
 
@@ -13,5 +16,21 @@ logger = logging.getLogger(__name__)
 class Image:
 
     def __init__(self, path: str, auto_make: bool = False) -> None:
+        self.path = realpath(expanduser(expandvars(normpath(path))))
+        if auto_make:
+            try:
+                makedirs(self.path, exist_ok=False)
+            except OSError:
+                raise OSError(
+                    '{} already exists, but auto_make = True'
+                    ''.format(self.path))
+            else:
+                self.bag = make_bag(self.path)
+        else:
+            try:
+                self.bag = Bag(self.path)
+            except BagError as e:
+                raise OSError(
+                    '{} does not seem to be a valid Moondog Image bag: {}'
+                    ''.format(self.path, str(e)))
         return None
-
--- tests/test_images.py
+++ tests/test_images.py
@@ -5,11 +5,14 @@
 import logging
 from moondog.images import Image as MoondogImage
 from nose.tools import assert_equal, assert_false, assert_true, raises
-from os.path import abspath, join, realpath
+from os import makedirs
+from os.path import abspath, exists, join, realpath
+from shutil import rmtree
 from unittest import TestCase
 
 logger = logging.getLogger(__name__)
 test_data_path = abspath(realpath(join('tests', 'data')))
+test_bag_path = join(test_data_path, 'foo')
 
 
 def setup_module():
@@ -22,18 +25,34 @@ def teardown_module():
     pass
 
 
-class Test_This(TestCase):
+class Test_Image_Basics(TestCase):
 
     def setUp(self):
         """Change me"""
-        pass
+        if exists(test_bag_path):
+            rmtree(test_bag_path)
 
     def tearDown(self):
-        """Change me"""
-        pass
+        """Delete temporary items"""
+        if exists(test_bag_path):
+            rmtree(test_bag_path)
 
-    def test_image(self):
-        """Test Image"""
-        im = MoondogImage(join(test_data_path, 'foo'))
+    def test_image_construct(self):
+        """Test Image Construction"""
+        im = MoondogImage(test_bag_path, auto_make=True)
         assert_true(isinstance(im, MoondogImage))
         del im
+
+    @raises(OSError)
+    def test_image_construct_exists(self):
+        makedirs(test_bag_path)
+        im = MoondogImage(test_bag_path, auto_make=True)
+        del im
+
+    @raises(OSError)
+    def test_image_construct_fail(self):
+        """Test Image Construction Failure (does not exist)"""
+        im = MoondogImage(test_bag_path)  # default: auto_make=False
+        del im
+
+
